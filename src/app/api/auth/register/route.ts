969ab202539a91f6e119a16adaeb9ae3c71adc0f
dhykas <mdhika987@gmail.com>
2024-08-26 18:45:35 +0700

    add register api

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,76 @@
+import { emailUnique, validation, ValidationReturn,  } from "@/utils/validation";
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest){
+    const prisma = new PrismaClient()
+
+    const formData = await req.formData();
+
+    interface objdata{
+        email: string,
+        password: string,
+        username: string,
+    }
+
+    const objData:objdata = Object.fromEntries(formData) as unknown as objdata;
+
+    const email: ValidationReturn = validation({
+        data: objData.email,
+        name: "email",
+        required: true,
+        minLength: 13
+    })
+    
+    const password: ValidationReturn = validation({
+        data: objData.password,
+        name: "password",
+        required: true,
+        minLength: 8,
+        maxLength: 28
+    });
+
+    const username: ValidationReturn = validation({
+        data: objData.username,
+        name: "username",
+        required: true,
+        minLength: 5,
+        maxLength: 20
+    });
+
+    if(email.error){
+        return NextResponse.json(email)
+    }
+
+    const uniqEmail = await emailUnique(email.data)
+
+    if(uniqEmail.error){
+        return NextResponse.json(uniqEmail, {
+            status: 400
+        })
+    }
+
+    if(username.error){
+        return NextResponse.json(username)
+    }
+
+    if(password.error){
+        return NextResponse.json(password)
+    }
+
+    const newUser = await prisma.user.create({
+        data: {
+            email: objData.email,
+            password: objData.password,
+            username: objData.username,
+        }
+    })
+
+    return NextResponse.json({
+        error: false,
+        message: 'New User Have Been Created',
+        newUser
+    }, {
+        status: 201
+    });
+}
---
+++ src/utils/validation.ts
@@ -0,0 +1,78 @@
+import { PrismaClient } from "@prisma/client"
+
+export interface ValidationReturn{
+    error: boolean,
+    message: string,
+    data: string
+}
+
+export async function emailUnique(email: string){
+    const prisma = new PrismaClient()
+
+    const emailUniq = await prisma.user.findFirst({
+        where: {
+            email
+        }
+    })
+
+    
+    if(emailUniq){
+        return {
+            data: email,
+            error: true,
+            message: "email have been used"
+        }
+    }
+    return {
+        data: email,
+        error: false,
+        message: "email is unique"
+    }
+}
+
+export function validation({
+    name,
+    minLength,
+    maxLength,
+    data,
+    required,
+}:{
+    name: string,
+    data:string,
+    required?: boolean,
+    minLength?: number,
+    maxLength?: number,
+}) : ValidationReturn
+{
+    if(required && !data){
+        return {
+            error: true,
+            message: name+" Is Required",
+            data
+        }
+
+    }
+
+    if(minLength && data.length < minLength){
+        return {
+            error: true,
+            message: name+" Should Be Longer Than "+minLength,
+            data
+        }
+    }
+
+    if(maxLength && data.length > maxLength){
+        return {
+            error: true,
+            message: name+" Should Be Less Than "+maxLength,
+            data
+        }
+    }
+
+    return {
+        error: false,
+        message: name+" is Valid",
+        data
+    }
+
+};
